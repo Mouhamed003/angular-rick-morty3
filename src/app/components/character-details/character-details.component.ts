400806f92f3fdbb54843b83fe5a9ac191c93e752
Mouhamed003 <mouhamedthiaw493@gmail.com>
2025-06-18 00:42:36 -0400

    Dernieres commits final

--- src/app/components/character-details/character-details.component.ts
+++ src/app/components/character-details/character-details.component.ts
@@ -1,11 +1,41 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RickAndMortyService } from '../../services/rick-and-morty.service';
 
 @Component({
   selector: 'app-character-details',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './character-details.component.html',
-  styleUrl: './character-details.component.scss'
 })
 export class CharacterDetailsComponent {
+  id!: number;
+  character: any;
+  errorMessage = '';
+  loading = false;
 
+  constructor(private rickAndMortyService: RickAndMortyService) {}
+
+  search() {
+    if (!Number.isInteger(this.id) || this.id <= 0) {
+      this.errorMessage = 'Veuillez entrer un ID entier positif.';
+      this.character = null;
+      return;
+    }
+
+    this.errorMessage = '';
+    this.loading = true;
+    this.rickAndMortyService.getCharacterById(this.id).subscribe({
+      next: (data: any) => {
+        this.character = data;
+        this.loading = false;
+      },
+      error: () => {
+        this.errorMessage = "Personnage non trouvé.";
+        this.character = null;
+        this.loading = false;
+      },
+    });
+  }
 }
--- src/app/components/characters-list/characters-list.component.ts
+++ src/app/components/characters-list/characters-list.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RickAndMortyService } from '../../services/rick-and-morty.service';
 
 @Component({
-  selector: 'app-characters-list',
-  imports: [],
+  
+  standalone: true,
+  imports: [CommonModule],
+   selector: 'app-characters-list',
   templateUrl: './characters-list.component.html',
-  styleUrl: './characters-list.component.scss'
 })
 export class CharactersListComponent {
+  characters: any[] = [];
 
+  constructor(private rickAndMortyService: RickAndMortyService) {
+    this.rickAndMortyService.getAllCharacters().subscribe(data => {
+      this.characters = data.results;
+    });
+  }
 }
